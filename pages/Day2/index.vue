d90d46bf09312ca3902181579d25a87075bb6588
hakuei0115 <king20010115@gmail.com>
2025-04-18 02:22:00 +0800

    feat: 新增 Day2 頁面，包含多種按鈕樣式與樣式表

---
+++ pages/Day2/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p class="title">Page: Day2/index</p>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+  <button type="button" class="btn btn-link">Link</button>
+  <button type="button" class="btn btn-outline-primary">Outline Primary</button>
+  <button type="button" class="btn btn-outline-secondary">Outline Secondary</button>
+  <button type="button" class="btn btn-outline-success">Outline Success</button>
+  <button type="button" class="btn btn-outline-danger">Outline Danger</button>
+  <button type="button" class="btn btn-outline-warning">Outline Warning</button>
+  <button type="button" class="btn btn-outline-info">Outline Info</button>
+  <button type="button" class="btn btn-outline-light">Outline Light</button>
+  <button type="button" class="btn btn-outline-dark">Outline Dark</button>
+</template>
+
+<style scoped></style>
