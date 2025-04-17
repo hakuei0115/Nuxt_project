3f572128fce044f50e97929595b788a2ccdd26ac
hakuei0115 <king20010115@gmail.com>
2025-04-18 02:27:12 +0800

    feat: 新增 Day3 頁面及其子頁面，包含前台與後台的導航連結

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>前台的表頭</header>
+  <slot />
+  <footer>前台的表尾</footer>
+</template>
+
+<style scoped></style>
---
+++ pages/Day3/admin/index.vue
@@ -0,0 +1,34 @@
+<script setup>
+  definePageMeta({
+    layout: 'admin'
+  })
+</script>
+
+<template>
+  <header>
+    <ul class="list">
+      <li>
+        <NuxtLink to="/Day3">前台首頁</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/Day3/about">前台 About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/Day3/admin">前往 後台首頁</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/Day3/admin/order">前往 後台訂單</NuxtLink>
+      </li>
+    </ul>
+  </header>
+  <h1>
+    後台首頁
+  </h1>
+  <footer>
+    <h5>
+      admin 模板的表尾
+    </h5>
+  </footer>
+</template>
+
+<style scoped></style>
