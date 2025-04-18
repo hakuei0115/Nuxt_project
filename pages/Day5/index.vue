cd93552cfe184eaad22f1cb0e428b7c3c7a35b86
hakueiMac <king20010115@gmail.com>
2025-04-18 16:46:49 +0800

    feat: 新增 Day5 頁面，包含最新消息的新聞卡片顯示與加載效果

---
+++ pages/Day5/index.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/css/index.css";
+const { newsList, isLoading, getNewList } = useHome();
+
+onMounted(() => {
+  getNewList();
+  isLoading.value = true;
+});
+
+definePageMeta({
+  layout: false,
+})
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>最新消息</h1>
+    <NewsCard v-for="list in newsList" :key="list._id"  v-bind="list" />
+
+    <ClientOnly>
+      <Loading v-model:active="isLoading" />
+    </ClientOnly>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+</style>
